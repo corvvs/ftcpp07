6adc72979d64efed93b5e8de25fac5641641d7f9
河田鷹介 <benefium@gmail.com>
2021-11-24 19:59:21 +0900

    output test

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <sstream>
 #include "Array.hpp"
 #include "array_tool.hpp"
 #include "Constants.hpp"
@@ -9,6 +10,46 @@
 
 void    say(const std::string& str) {
     std::cout << str << std::endl;
+}
+
+std::stringstream ss;
+
+template <typename Result>
+void    tester(
+    const std::string& title,
+    const Result& result,
+    const Result& expected,
+    const bool ignorable = false
+) {
+    bool ok = result == expected;
+    const std::string color =
+        ok
+            ? Constants::kTextInfo
+            : ignorable
+                ? Constants::kTextWarning
+                : Constants::kTextError;
+    std::cout
+        << color
+        << ((result == expected) ? "[ok] " : "[KO] ")
+        << title
+        << ": "
+        << std::boolalpha << result;
+    if (!ok) {
+        std::cout
+            << "(expected: " << expected << ")";
+    }
+    std::cout
+        << Constants::kTextReset << std::endl;
+    if (!ok && !ignorable) {
+        throw std::string("** detected KO test **");
+    }
+}
+
+std::string stringify(void) {
+    std::string res;
+    res = ss.str();
+    ss.str("");
+    return res;
 }
 
 template<class T>
@@ -46,31 +87,36 @@ bool is_hello(std::string& str) {
 int main(int argc, char **argv) {
     try {
         say("[ Array<int> ]");
-        int n = 10;
+        unsigned int n = 10;
         Array<int> int_array(n);
-        for (int i = 0; i < n; i += 1) {
+        for (unsigned i = 0; i < n; i += 1) {
             int_array[i] = i + i * i + 1;
         }
         Array<int> int_array2(1);
-        std::cout << int_array.getSize() << std::endl;
-        std::cout << int_array2.getSize() << std::endl;
-        std::cout << int_array[0] << std::endl;
-        std::cout << int_array2[0] << std::endl;
+        tester("size of int_array(10)", int_array.getSize(), n);
+        tester("int_array[0]", int_array[0], 1);
+        tester("size of int_array2(1)", int_array2.getSize(), 1u);
+        tester("int_array2[0]", int_array2[0], 0);
         int_array[0] = 42;
         int_array2 = int_array;
         int_array2[0] = 24;
-        std::cout << int_array.getSize() << std::endl;
-        std::cout << int_array2.getSize() << std::endl;
-        std::cout << int_array << std::endl;
-        std::cout << int_array2 << std::endl;
+        say("int_array[0] = 42, int_array2 = int_array, int_array2[0] = 24");
+        tester("size of int_array", int_array.getSize(), n);
+        tester("size of int_array2", int_array2.getSize(), n);
+        ss << int_array;
+        tester("int_array", stringify(), std::string("[42, 3, 7, 13, 21, 31, 43, 57, 73, 91]"));
+        ss << int_array2;
+        tester("int_array2", stringify(), std::string("[24, 3, 7, 13, 21, 31, 43, 57, 73, 91]"));
+        say("int_array3 is cloned from int_array2");
         Array<int> int_array3(int_array2);
-        std::cout << int_array3.getSize() << std::endl;
-        std::cout << int_array3 << std::endl;
-        std::cout << std::endl;
+        tester("size of int_array3", int_array3.getSize(), n);
+        ss << int_array3;
+        tester("int_array3", stringify(), std::string("[24, 3, 7, 13, 21, 31, 43, 57, 73, 91]"));
+        say("int_array4 is cloned from int_array");
         const Array<int> int_array4(int_array);
-        std::cout << int_array4.getSize() << std::endl;
-        std::cout << int_array4 << std::endl;
-        std::cout << std::endl;
+        tester("size of int_array4", int_array3.getSize(), n);
+        ss << int_array4;
+        tester("int_array4", stringify(), std::string("[42, 3, 7, 13, 21, 31, 43, 57, 73, 91]"));
     } catch (std::exception& e) {
         std::cout
             << Constants::kTextError
@@ -119,6 +165,7 @@ int main(int argc, char **argv) {
     try {
         say("[ out of range ]");
         Array<int> array(10);
+        say("reading index 10 from size-10 array, it will cause EXCEPTION");
         std::cout << array[10] << std::endl;
     } catch (std::exception& e) {
         std::cout
@@ -132,11 +179,15 @@ int main(int argc, char **argv) {
         Array<int> array = Array<int>(10);
         array[0] = 100;
         array[9] = -42;
-        std::cout << array << std::endl;
+        ss << array;
+        tester("array", stringify(), std::string("[100, 0, 0, 0, 0, 0, 0, 0, 0, -42]"));
         Array<int>* ap = &array;
-        *ap = array;
-        std::cout << array << std::endl;
-        std::cout << *ap << std::endl;
+        say("ap = &array");
+        say("*ap = array (self assignation)");
+        ss << array;
+        tester("array", stringify(), std::string("[100, 0, 0, 0, 0, 0, 0, 0, 0, -42]"));
+        ss << *ap;
+        tester("ap", stringify(), std::string("[100, 0, 0, 0, 0, 0, 0, 0, 0, -42]"));
     } catch (std::exception& e) {
         std::cout
             << Constants::kTextError
